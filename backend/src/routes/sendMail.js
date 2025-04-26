643c25fb33c9fee8f58569e69414fc29282f8deb
vikas5126 <vikas18046171@gmail.com>
2025-04-26 09:39:21 +0530

    remove backend form the same folder

--- backend/src/routes/sendMail.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import sendMail from '../controller/sendMail.js';
-
-const router = express.Router();
-
-router.post('/send-email', sendMail)
-
-export default router;
-
